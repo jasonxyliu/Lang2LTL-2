100010eac97916059444257be41092160bf312ef
davidpaulius <paulius.david@gmail.com>
2024-02-03 02:16:03 -0500

    Updated generate_dataset() function

--- utils.py
+++ utils.py
@@ -7,6 +7,7 @@ import re
 
 from random import randint, choice
 from pandas import read_csv
+from tqdm import tqdm
 
 def deserialize_props_str(props_str):
     """
@@ -62,7 +63,7 @@ def save_to_file(data, fpth, mode=None):
         raise ValueError(f"ERROR: file type {ftype} not recognized")
 
 
-def generate_dataset(params, fpath, num_utterances=10, min_props=2):
+def generate_dataset(params, utts_fpath, gtr_fpath, num_utterances=10, min_props=2):
 
     def load_ltl_samples(fpath):
         return read_csv(open(fpath, 'r'))
@@ -79,24 +80,21 @@ def generate_dataset(params, fpath, num_utterances=10, min_props=2):
     landmarks = list(gtr.keys())
 
     list_utterances = []
-    used_templates = []
+    list_true_results = []
 
     count = 0
 
-    while count < num_utterances:
+    for N in tqdm(range(num_utterances), desc="Generating synthetic dataset and groundtruth file..."):
         # -- use the eval function to get the list of props for a randomly selected row from the set of LTL blueprints:
         ltl_sample = ltl_samples.iloc[randint(0, len(ltl_samples)-1)]
-        ltl_props = eval(ltl_sample['props'])
+        ltl_props = list(set(eval(ltl_sample['props'])))
 
         if len(ltl_props) < min_props:
+            N -= 1
             continue
 
-        count += 1
-
         ltl_blueprint = ltl_sample['utterance']
-
-        # -- save all original templates for matching them to reverse-engineered result later:
-        used_templates.append(str(ltl_blueprint))
+        ltl_formula = ltl_sample['ltl_formula']
 
         if not ltl_blueprint.endswith('.'):
             ltl_blueprint += '.'
@@ -105,23 +103,51 @@ def generate_dataset(params, fpath, num_utterances=10, min_props=2):
         if not ltl_blueprint.startswith('.'):
             ltl_blueprint = '.' + ltl_blueprint
 
+        list_true_srer = []
+        list_true_sre = []
+
         new_command = ltl_blueprint
         for x in range(len(ltl_props)):
 
             target = choice(landmarks)
 
-            random_pred = choice(gtr[target])
+            new_sre = None
+            while not bool(new_sre):
+                random_pred = choice(gtr[target])
+                # -- this is an element without any spatial relation:
+                if "*" in random_pred:
+                    new_sre = random_pred["*"]
+                    random_pred = {new_sre : {}}
+                else:
+                    rel = list(random_pred.keys())[0]
+                    if len(random_pred[rel]) == 2:
+                        new_sre = f'{random_pred[rel][0]} {rel} {random_pred[rel][1]}'
+                    elif len(random_pred[rel]) == 3:
+                        new_sre = f'{random_pred[rel][0]} {rel} {random_pred[rel][1]} and {random_pred[rel][2]}'
+
+            list_true_sre.append(new_sre)
+            list_true_srer.append(random_pred)
 
             # NOTE: to do replacement of the lifted proposition with the generated one, we need to account for
             # different ways it would be written preceded by a whitespace character, i.e., ' a ', ' a,', ' a.'
-            new_command = re.sub(rf"(\W)([{ltl_props[x]}])(\W)", rf'\1{random_pred}\3', new_command)
+            new_command = re.sub(rf"(\W)([{ltl_props[x]}])(\W)", rf'\1{new_sre}\3', new_command)
 
             # NOTE: some utterances will be missing some propositions
 
         list_utterances.append(new_command[1:-1])
 
+        list_true_results.append({
+            "utt": list_utterances[-1],
+            "true_ltl": ltl_formula,
+            "true_srer": list_true_srer,
+            "true_sre": list_true_sre
+        })
+
     utts_for_file = ""
     for command in list_utterances:
         utts_for_file = f'{command}\n{utts_for_file}'
 
-    save_to_file(utts_for_file, fpath)
+    save_to_file(utts_for_file, utts_fpath)
+    save_to_file(list_true_results, gtr_fpath)
+
+    print()
