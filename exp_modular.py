02b92af827a8e11d4f5dad3881ceb78a945ee4d6
Jason Xinyu Liu <xinyu_liu@brown.edu>
2024-02-26 12:52:57 -0500

    added log

--- exp_modular.py
+++ exp_modular.py
@@ -129,6 +129,8 @@ def eval_spg(true_results_fpath, graph_dpath, osm_fpath, topk, rel_embeds_fpath,
                     for (lmk_type_true, ground_true), (lmk_type_out, ground_out) in zip(sp_true[0].items(), sp_out.items()):
                         if lmk_type_out != lmk_type_true or ground_out != ground_true:
                             is_correct = False
+                            if end_idx == 1:
+                                logging.info(f"Incorrect Top-1 spatial predicate grounding: \n{sre_true}\ntrue: {lmk_type_true}; {ground_true}\npred: {lmk_type_out}; {ground_out}")
 
                     if is_correct:
                         topk2acc[end_idx] += 1
