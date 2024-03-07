596428289f8eae56101c9efbc450910103a25c00
Jason Xinyu Liu <xinyu_liu@brown.edu>
2024-03-07 18:35:56 -0500

    updated arg name in bash

--- bash_scripts/run_exp_modular.bash
+++ bash_scripts/run_exp_modular.bash
@@ -1,34 +1,34 @@
 #!/bin/bash
 
 # all auckland runs:
-python3 synthetic_dataset.py --location "auckland" --nsamples 10 --seed 1
-python3 synthetic_dataset.py --location "auckland" --nsamples 10 --seed 2
-python3 synthetic_dataset.py --location "auckland" --nsamples 10 --seed 42
-python3 synthetic_dataset.py --location "auckland" --nsamples 10 --seed 111
+python3 synthetic_dataset.py --loc "auckland" --nsamples 10 --seed 1
+python3 synthetic_dataset.py --loc "auckland" --nsamples 10 --seed 2
+python3 synthetic_dataset.py --loc "auckland" --nsamples 10 --seed 42
+python3 synthetic_dataset.py --loc "auckland" --nsamples 10 --seed 111
 
-nohup python3 exp_modular.py --location "auckland" --module "all" --nsamples 10 --seed 1 > /dev/null 2>&1 &
-nohup python3 exp_modular.py --location "auckland" --module "all" --nsamples 10 --seed 2 > /dev/null 2>&1 &
-nohup python3 exp_modular.py --location "auckland" --module "all" --nsamples 10 --seed 42 > /dev/null 2>&1 &
-nohup python3 exp_modular.py --location "auckland" --module "all" --nsamples 10 --seed 111 > /dev/null 2>&1 &
+nohup python3 exp_modular.py --loc "auckland" --module "all" --nsamples 10 --seed 1 > /dev/null 2>&1 &
+nohup python3 exp_modular.py --loc "auckland" --module "all" --nsamples 10 --seed 2 > /dev/null 2>&1 &
+nohup python3 exp_modular.py --loc "auckland" --module "all" --nsamples 10 --seed 42 > /dev/null 2>&1 &
+nohup python3 exp_modular.py --loc "auckland" --module "all" --nsamples 10 --seed 111 > /dev/null 2>&1 &
 
 # all blackstone runs:
-python3 synthetic_dataset.py --location "blackstone" --nsamples 10 --seed 1
-python3 synthetic_dataset.py --location "blackstone" --nsamples 10 --seed 2
-python3 synthetic_dataset.py --location "blackstone" --nsamples 10 --seed 42
-python3 synthetic_dataset.py --location "blackstone" --nsamples 10 --seed 111
+python3 synthetic_dataset.py --loc "blackstone" --nsamples 10 --seed 1
+python3 synthetic_dataset.py --loc "blackstone" --nsamples 10 --seed 2
+python3 synthetic_dataset.py --loc "blackstone" --nsamples 10 --seed 42
+python3 synthetic_dataset.py --loc "blackstone" --nsamples 10 --seed 111
 
-nohup python3 exp_modular.py --location "blackstone" --module "all" --nsamples 10 --seed 1 > /dev/null 2>&1 &
-nohup python3 exp_modular.py --location "blackstone" --module "all" --nsamples 10 --seed 2 > /dev/null 2>&1 &
-nohup python3 exp_modular.py --location "blackstone" --module "all" --nsamples 10 --seed 42 > /dev/null 2>&1 &
-nohup python3 exp_modular.py --location "blackstone" --module "all" --nsamples 10 --seed 111 > /dev/null 2>&1 &
+nohup python3 exp_modular.py --loc "blackstone" --module "all" --nsamples 10 --seed 1 > /dev/null 2>&1 &
+nohup python3 exp_modular.py --loc "blackstone" --module "all" --nsamples 10 --seed 2 > /dev/null 2>&1 &
+nohup python3 exp_modular.py --loc "blackstone" --module "all" --nsamples 10 --seed 42 > /dev/null 2>&1 &
+nohup python3 exp_modular.py --loc "blackstone" --module "all" --nsamples 10 --seed 111 > /dev/null 2>&1 &
 
 # all boston runs:
-python3 synthetic_dataset.py --location "boston" --nsamples 10 --seed 1
-python3 synthetic_dataset.py --location "boston" --nsamples 10 --seed 2
-python3 synthetic_dataset.py --location "boston" --nsamples 10 --seed 42
-python3 synthetic_dataset.py --location "boston" --nsamples 10 --seed 111
+python3 synthetic_dataset.py --loc "boston" --nsamples 10 --seed 1
+python3 synthetic_dataset.py --loc "boston" --nsamples 10 --seed 2
+python3 synthetic_dataset.py --loc "boston" --nsamples 10 --seed 42
+python3 synthetic_dataset.py --loc "boston" --nsamples 10 --seed 111
 
-nohup python3 exp_modular.py --location "boston" --module "all" --nsamples 10 --seed 1 > /dev/null 2>&1 &
-nohup python3 exp_modular.py --location "boston" --module "all" --nsamples 10 --seed 2 > /dev/null 2>&1 &
-nohup python3 exp_modular.py --location "boston" --module "all" --nsamples 10 --seed 42 > /dev/null 2>&1 &
-nohup python3 exp_modular.py --location "boston" --module "all" --nsamples 10 --seed 111 > /dev/null 2>&1 &
+nohup python3 exp_modular.py --loc "boston" --module "all" --nsamples 10 --seed 1 > /dev/null 2>&1 &
+nohup python3 exp_modular.py --loc "boston" --module "all" --nsamples 10 --seed 2 > /dev/null 2>&1 &
+nohup python3 exp_modular.py --loc "boston" --module "all" --nsamples 10 --seed 42 > /dev/null 2>&1 &
+nohup python3 exp_modular.py --loc "boston" --module "all" --nsamples 10 --seed 111 > /dev/null 2>&1 &
