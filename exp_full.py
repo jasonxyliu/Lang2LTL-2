f513d7251b07041d5c80cf645dd7b1ab3f4f5258
Jason Xinyu Liu <xinyu_liu@brown.edu>
2024-03-13 01:18:02 -0400

    fixed arg choice

--- exp_full.py
+++ exp_full.py
@@ -106,7 +106,7 @@ def eval_full_system(true_results_fpath, lt_out_fpath):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--loc", type=str, default="providence", choices=["providence", "boston", "auckland", "san_francisco"], help="env name.")
-    parser.add_argument("--ablate", type=str, default="image", choices=["image", "image", None], help="ablate out a modality or None to use both.")
+    parser.add_argument("--ablate", type=str, default="text", choices=["text", "image", None], help="ablate out a modality or None to use both.")
     parser.add_argument("--nsamples", type=int, default=3, help="number of sample utts per LTL formula or None for all")
     parser.add_argument("--seed", type=int, default=111, help="seed to random sampler.")  # 0, 1, 2, 42, 111
     parser.add_argument("--topk", type=int, default=10, help="top k most likely landmarks grounded by REG.")
