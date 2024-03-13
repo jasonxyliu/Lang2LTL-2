f510ec127fe471cb48a7e402ee19f8de8d638dd1
Jason Xinyu Liu <xinyu_liu@brown.edu>
2024-03-12 23:56:34 -0400

    added construct ablate dataset

--- dataset_ablate.py
+++ dataset_ablate.py
@@ -1,7 +1,6 @@
 import os
 import argparse
 import logging
-from pathlib import Path
 from collections import defaultdict
 import itertools
 import random
@@ -21,60 +20,59 @@ def split_true_lmk_grounds(loc, lmks_fpath, obj_fpath, sp_fpath, res_fpath):
     lmk_ids = [lmk_id for lmk_id in lmk_grounds.keys() if lmk_id not in obj_ids]  # landmarks described by text
 
     sp_grounds = {"text": defaultdict(list), "image": defaultdict(list), "both": defaultdict(list)}
-    res = {"text": defaultdict(lambda: defaultdict(list)), "image": defaultdict(lambda: defaultdict(list)), "both": defaultdict(lambda: defaultdict(list))}
+    res = defaultdict(lambda: defaultdict(list))
 
     for lmk, grounds in lmk_grounds.items():
         for ground in grounds:
             if "*" in ground:  # unique referring expression can identify landmark without anchor
-                modal = "text" if lmk in lmk_ids else "image"
-                res[modal][lmk]["proper_names"].append(ground["*"])
-                if lmk not in sp_grounds[modal]["None"]:
-                    sp_grounds[modal]["None"].append(lmk)
+                modality = "text" if lmk in lmk_ids else "image"
+                res[lmk]["proper_names"].append(ground["*"])
+                if lmk not in sp_grounds[modality]["None"]:
+                    sp_grounds[modality]["None"].append(lmk)
             elif "@" in ground:  # ambiguous referring expression if used without anchor
-                modal = "text" if lmk in lmk_ids else "image"
-                res[modal][lmk]["generic_names"].append(ground["@"])
+                res[lmk]["generic_names"].append(ground["@"])
             else:  # referring expression grounding
                 rel = list(ground.keys())[0]
 
                 if len(ground[rel]) == 2:
                     if ground[rel][0][0] in lmk_ids:
                         if ground[rel][1][0] in lmk_ids:
-                            modal = "text"
+                            modality = "text"
                         else:
-                            modal = "both"
+                            modality = "both"
                     else:
                         if ground[rel][1][0] in lmk_ids:
-                            modal = "both"
+                            modality = "both"
                         else:
-                            modal = "image"
+                            modality = "image"
                 elif len(ground[rel]) == 3:
                     if ground[rel][0][0] in lmk_ids:
                         if ground[rel][1][0] in lmk_ids:
                             if ground[rel][2][0] in lmk_ids:
-                                modal = "text"
+                                modality = "text"
                             else:
-                                modal = "both"
+                                modality = "both"
                         else:
-                            modal = "both"
+                            modality = "both"
                     else:
                         if ground[rel][1][0] in lmk_ids:
-                            modal = "both"
+                            modality = "both"
                         else:
                             if ground[rel][2][0] in lmk_ids:
-                                modal = "image"
+                                modality = "image"
                             else:
-                                modal = "both"
+                                modality = "both"
                 else:
                     raise IndexError(f"Incorrect number of RE grounds (must be 2 or 3): {ground[rel]}")
 
-                if ground[rel] not in sp_grounds[modal][rel]:
-                    sp_grounds[modal][rel].append(ground[rel])
+                if ground[rel] not in sp_grounds[modality][rel]:
+                    sp_grounds[modality][rel].append(ground[rel])
 
     save_to_file(sp_grounds, sp_fpath)
     save_to_file(res, res_fpath)
 
 
-def generate_dataset(ltl_fpath, obj_fpath, osm_fpath, sp_fpath, res_fpath, utts_fpath, outs_fpath, nsamples, seed):
+def construct_dataset(ltl_fpath, obj_fpath, osm_fpath, sp_fpath, res_fpath, utts_fpath, outs_fpath, nsamples, seed):
     """
     Generate input utterances and ground truth results for each grounding module.
     """
@@ -97,7 +95,8 @@ def generate_dataset(ltl_fpath, obj_fpath, osm_fpath, sp_fpath, res_fpath, utts_
         nutts += len(data)
         logging.info(f"{ltl}: {len(data)}")
     logging.info(f"# unique utterances: {nutts}")
-    logging.info(f"# unique spatial relations: {len(sp_grounds_all)}")
+    modality2ngrounds = {modality: len(grounds) for modality, grounds in sp_grounds_all.items()}
+    logging.info(f"# unique spatial relations: {modality2ngrounds}")
     logging.info(f"# unique landmarks: {len(res_all)}")
 
     utts = ""
@@ -105,81 +104,96 @@ def generate_dataset(ltl_fpath, obj_fpath, osm_fpath, sp_fpath, res_fpath, utts_
 
     for ltl_lifted, ltl_data in ltl2data:  # every lifted LTL formula
         data_sampled = random.sample(sorted(ltl_data), nsamples) if nsamples else sorted(ltl_data)
-        for data in data_sampled:  # every sampled lifted utterances
-            pattern_type, props_full_str, utt_lifted = data
-            props_full = eval(props_full_str)
-            props = [props_full[0]] if len(set(props_full)) == 1 else props_full  # e.g., visit a at most twice, ['a', 'a']
-            rels = random.sample(sorted(sp_grounds_all), len(props))
+        ratio = len(data_sampled) // 3  # text only, image only, both modality
 
-            sre_to_preds = {}
-            grounded_sre_to_preds = defaultdict(dict)
-            grounded_sps = defaultdict(list)
-
-            for rel in rels:  # every sampled spatial relations
-                sp_grounds_sampled = random.sample(sp_grounds_all[rel], 1)[0]
-                res_true = []
-
-                if rel == "None":  # referring expression without spatial relation
-                    sre = random.sample(res_all[sp_grounds_sampled]["proper_names"], 1)[0]
-                    res_true.append(sre)
-                    sp_true = {"target": sp_grounds_sampled}
-                elif len(sp_grounds_sampled) == 1:  # sre with only an anchor
-                    while "proper_names" not in res_all[sp_grounds_sampled[0]]:
-                        sp_grounds_sampled = random.sample(sp_grounds_all[rel], 1)[0]
-                    re_tar = random.sample(res_all[sp_grounds_sampled[0]]["proper_names"], 1)[0]
-                    res_true.append(re_tar)
-                    sre = f"{rel} {re_tar}"
-                    sp_true = {"anchor": [sp_grounds_sampled[0]]}
-                else:  # for sre with target and one or two anchors, both proper and generic names are valid
-                    while "proper_names" not in res_all[sp_grounds_sampled[1][0]] \
-                        or (len(sp_grounds_sampled) == 3 and "proper_names" not in res_all[sp_grounds_sampled[2][0]]):
-                        sp_grounds_sampled = random.sample(sp_grounds_all[rel], 1)[0]
-                    res_tar = list(itertools.chain.from_iterable(res_all[sp_grounds_sampled[0][0]].values()))
-                    re_tar = random.sample(res_tar, 1)[0]  # target referring expression
-                    res_true.append(re_tar)
-                    re_anc1 = random.sample(res_all[sp_grounds_sampled[1][0]]["proper_names"], 1)[0]  # anchor 1 referring expression
-                    res_true.append(re_anc1)
-                    if len(sp_grounds_sampled) == 2:
-                        sre = f"{re_tar} {rel} {re_anc1}"
-                        sp_true = {"target": sp_grounds_sampled[0][0], "anchor": [sp_grounds_sampled[1][0]]}
-                    else:
-                        re_anc2 = random.sample(res_all[sp_grounds_sampled[2][0]]["proper_names"], 1)[0] # anchor 2 referring expression
-                        res_true.append(re_anc2)
-                        sre = f"{re_tar} {rel} {re_anc1} and {re_anc2}"
-                        sp_true = {"target": sp_grounds_sampled[0][0], "anchor": [sp_grounds_sampled[1][0], sp_grounds_sampled[2][0]]}
-
-                sre_to_preds[sre] = {rel: res_true}
-
-                if rel == "None":
-                    grounded_sre_to_preds[sre][rel] = [[[1.0, sp_grounds_sampled]]]
-                else:
-                    grounded_sre_to_preds[sre][rel] = [[score_ground] for score_ground in [[1.0, sp_ground[0]] for sp_ground in sp_grounds_sampled]]
-
-                grounded_sps[sre].append(sp_true)
-
-            if not utt_lifted.startswith('.'):
-                utt_ground = '.' + utt_lifted
-            if not utt_ground.endswith('.'):
-                utt_ground += '.'
-            for prop, sre in zip(props, sre_to_preds.keys()):
-                utt_ground = re.sub(rf"(\b)([{prop}])(\W)", rf'\1{sre}\3', utt_ground)
-            utt_ground = utt_ground[1:-1]
-            utts += f"{utt_ground}\n"
-
-            true_outs.append({
-                "pattern_type": pattern_type,
-                "utt": utt_ground.strip(),
-                "lifted_utt": utt_lifted,
-                "props": props_full,
-                "sre_to_preds": sre_to_preds,
-                "grounded_sre_to_preds": grounded_sre_to_preds,
-                "grounded_sps": grounded_sps,
-                "lifted_ltl": ltl_lifted
-            })
+        for data in data_sampled[: ratio]:
+            modality = "text"
+            utts = construct_utt(modality, data, sp_grounds_all[modality], res_all, utts, true_outs, ltl_lifted)
+        for data in data_sampled[ratio: ratio + ratio]:
+            modality = "image"
+            utts = construct_utt(modality, data, sp_grounds_all[modality], res_all, utts, true_outs, ltl_lifted)
+        for data in data_sampled[ratio + ratio:]:
+            modality = "both"
+            utts = construct_utt(modality, data, sp_grounds_all[modality], res_all, utts, true_outs, ltl_lifted)
 
     save_to_file(utts, utts_fpath)
     save_to_file(true_outs, outs_fpath)
     logging.info(f"# data points: {len(true_outs)}")
+
+
+def construct_utt(modality, data, sp_grounds_all, res_all, utts, true_outs, ltl_lifted):
+    pattern_type, props_full_str, utt_lifted = data
+    props_full = eval(props_full_str)
+    props = [props_full[0]] if len(set(props_full)) == 1 else props_full  # e.g., visit a at most twice, ['a', 'a']
+    rels = random.sample(sorted(sp_grounds_all), len(props))
+
+    sre_to_preds = {}
+    grounded_sre_to_preds = defaultdict(dict)
+    grounded_sps = defaultdict(list)
+
+    for rel in rels:  # every sampled spatial relations
+        sp_grounds_sampled = random.sample(sp_grounds_all[rel], 1)[0]
+        res_true = []
+
+        if rel == "None":  # referring expression without spatial relation
+            sre = random.sample(res_all[sp_grounds_sampled]["proper_names"], 1)[0]
+            res_true.append(sre)
+            sp_true = {"target": sp_grounds_sampled}
+        elif len(sp_grounds_sampled) == 1:  # sre with only an anchor
+            while "proper_names" not in res_all[sp_grounds_sampled[0]]:
+                sp_grounds_sampled = random.sample(sp_grounds_all[rel], 1)[0]
+            re_tar = random.sample(res_all[sp_grounds_sampled[0]]["proper_names"], 1)[0]
+            res_true.append(re_tar)
+            sre = f"{rel} {re_tar}"
+            sp_true = {"anchor": [sp_grounds_sampled[0]]}
+        else:  # for sre with target and one or two anchors, both proper and generic names are valid
+            while "proper_names" not in res_all[sp_grounds_sampled[1][0]] \
+                or (len(sp_grounds_sampled) == 3 and "proper_names" not in res_all[sp_grounds_sampled[2][0]]):
+                sp_grounds_sampled = random.sample(sp_grounds_all[rel], 1)[0]
+            res_tar = list(itertools.chain.from_iterable(res_all[sp_grounds_sampled[0][0]].values()))
+            re_tar = random.sample(res_tar, 1)[0]  # target referring expression
+            res_true.append(re_tar)
+            re_anc1 = random.sample(res_all[sp_grounds_sampled[1][0]]["proper_names"], 1)[0]  # anchor 1 referring expression
+            res_true.append(re_anc1)
+            if len(sp_grounds_sampled) == 2:
+                sre = f"{re_tar} {rel} {re_anc1}"
+                sp_true = {"target": sp_grounds_sampled[0][0], "anchor": [sp_grounds_sampled[1][0]]}
+            else:
+                re_anc2 = random.sample(res_all[sp_grounds_sampled[2][0]]["proper_names"], 1)[0] # anchor 2 referring expression
+                res_true.append(re_anc2)
+                sre = f"{re_tar} {rel} {re_anc1} and {re_anc2}"
+                sp_true = {"target": sp_grounds_sampled[0][0], "anchor": [sp_grounds_sampled[1][0], sp_grounds_sampled[2][0]]}
+
+        sre_to_preds[sre] = {rel: res_true}
+
+        if rel == "None":
+            grounded_sre_to_preds[sre][rel] = [[[1.0, sp_grounds_sampled]]]
+        else:
+            grounded_sre_to_preds[sre][rel] = [[score_ground] for score_ground in [[1.0, sp_ground[0]] for sp_ground in sp_grounds_sampled]]
+
+        grounded_sps[sre].append(sp_true)
+
+    if not utt_lifted.startswith('.'):
+        utt_ground = '.' + utt_lifted
+    if not utt_ground.endswith('.'):
+        utt_ground += '.'
+    for prop, sre in zip(props, sre_to_preds.keys()):
+        utt_ground = re.sub(rf"(\b)([{prop}])(\W)", rf'\1{sre}\3', utt_ground)
+    utt_ground = utt_ground[1:-1]
+    utts += f"{utt_ground}\n"
+
+    true_outs.append({
+        "pattern_type": pattern_type,
+        "modality": modality,
+        "utt": utt_ground.strip(),
+        "lifted_utt": utt_lifted,
+        "props": props_full,
+        "sre_to_preds": sre_to_preds,
+        "grounded_sre_to_preds": grounded_sre_to_preds,
+        "grounded_sps": grounded_sps,
+        "lifted_ltl": ltl_lifted
+    })
+    return utts
 
 
 if __name__ == "__main__":
@@ -191,17 +205,16 @@ if __name__ == "__main__":
     loc_id = f"{args.loc}_n{args.nsamples}_seed{args.seed}" if args.nsamples else f"{args.loc}_all_seed{args.seed}"
 
     data_dpath = os.path.join(os.path.expanduser("~"), "ground", "data")
-    # img_dpath = os.path.join(data_dpath, "maps", LOC2GID[args.loc], "images")
-    obj_fpath = os.path.join(data_dpath, "maps", LOC2GID[args.loc], "obj_locs.json")
-    osm_fpath = os.path.join(data_dpath, "osm", f"{args.loc}.json")
+    obj_fpath = os.path.join(data_dpath, "maps", f"{LOC2GID[args.loc]}_ablate", "obj_locs.json")
+    osm_fpath = os.path.join(data_dpath, "osm_ablate", f"{args.loc}.json")
     dataset_dpath = os.path.join(os.path.expanduser("~"), "ground", "data", "dataset")
-    loc_dpath = os.path.join(dataset_dpath, args.loc)
+    loc_dpath = os.path.join(dataset_dpath, f"{args.loc}_ablate")
     os.makedirs(loc_dpath, exist_ok=True)
     ltl_fpath = os.path.join(dataset_dpath, "ltl_samples_sorted.csv")
-    sp_fpath = os.path.join(loc_dpath, f"{args.loc}_sp_grounds_ablate.json")
-    res_fpath = os.path.join(loc_dpath, f"{args.loc}_res_ablate.json")
-    utts_fpath = os.path.join(loc_dpath, f"{loc_id}_utts_ablate.txt")
-    outs_fpath = os.path.join(loc_dpath, f"{loc_id}_true_results_ablate.json")
+    sp_fpath = os.path.join(loc_dpath, f"{args.loc}_sp_grounds.json")
+    res_fpath = os.path.join(loc_dpath, f"{args.loc}_res.json")
+    utts_fpath = os.path.join(loc_dpath, f"{loc_id}_utts.txt")
+    outs_fpath = os.path.join(loc_dpath, f"{loc_id}_true_results.json")
 
     logging.basicConfig(level=logging.INFO,
                         format='%(message)s',
@@ -219,4 +232,4 @@ if __name__ == "__main__":
     split_true_lmk_grounds(args.loc, lmks_fpath, obj_fpath, sp_fpath, res_fpath)
 
     # if not os.path.isfile(utts_fpath) or not os.path.isfile(outs_fpath):
-    #     generate_dataset(ltl_fpath, obj_fpath, osm_fpath, sp_fpath, res_fpath, utts_fpath, outs_fpath, args.nsamples, args.seed)
+    construct_dataset(ltl_fpath, obj_fpath, osm_fpath, sp_fpath, res_fpath, utts_fpath, outs_fpath, args.nsamples, args.seed)
