e239505ff90792d927bccc5fe4c5a33c73f96401
Jason Xinyu Liu <xinyu_liu@brown.edu>
2024-03-18 00:15:25 -0400

    added code alayze results: srer, reg

--- analyze_results.py
+++ analyze_results.py
@@ -4,86 +4,72 @@ Modular-wise Evaluation: correct input to each module, check modular output.
 import os
 import argparse
 import logging
-from shutil import copy2
+from collections import defaultdict
 
-from ground import LOC2GID
-from srer import run_exp_srer
-from reg import run_exp_reg
-from spg import run_exp_spg
-from lt import run_exp_lt
-from lt_rag import run_exp_lt_rag
 from evaluate import eval_srer, eval_reg, eval_spg, eval_lt
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--module", type=str, default="srer", choices=["srer", "reg", "spg", "lt", "all"], help="domain name.")
-    parser.add_argument("--loc", type=str, default="providence", choices=["providence", "auckland", "boston", "san_francisco"], help="domain name.")
+    parser.add_argument("--module", type=str, default="reg", choices=["srer", "reg", "spg", "lt", "all"], help="domain name.")
     parser.add_argument("--ablate", type=str, default=None, choices=["both", "image", "text", None], help="ablate out a modality.")
     parser.add_argument("--nsamples", type=int, default=None, help="number of sample utts per LTL formula or None for all.")
-    parser.add_argument("--seed", type=int, default=0, help="seed to random sampler.")  # 0, 1, 2, 42, 111 (resreved for ablate)
     parser.add_argument("--topk", type=int, default=10, help="top k most likely landmarks grounded by REG.")
     parser.add_argument("--lt", type=str, default="t5", choices=["t5", "rag"], help="lifted translation model.")
     parser.add_argument("--nexamples", type=int, default=2, help="number of in-context examples if use RAG lifted translation model.")
     args = parser.parse_args()
-    loc_id = f"{args.loc}_n{args.nsamples}_seed{args.seed}" if args.nsamples else f"{args.loc}_all_seed{args.seed}"
-    lt_id = f"lt-{args.lt}{args.nexamples}" if args.lt == "rag" else f"{args.lt}"
-
-    data_dpath = os.path.join(os.path.expanduser("~"), "ground", "data")
-    graph_dpath = os.path.join(data_dpath, "maps", f"{LOC2GID[args.loc]}_ablate" if args.ablate else LOC2GID[args.loc])
-    osm_fpath = os.path.join(data_dpath, "osm_ablate" if args.ablate else "osm", f"{args.loc}.json")
-    utts_fpath = os.path.join(data_dpath, "dataset", f"{args.loc}_ablate" if args.ablate else f"{args.loc}", f"{loc_id}_utts.txt")
-    model_fpath = os.path.join(os.path.expanduser("~"), "ground", "models", "checkpoint-best")
-    rel_embeds_fpath = os.path.join(data_dpath, f"known_rel_embeds.json")
-    reg_in_cache_fpath = os.path.join(data_dpath, f"reg_in_cache_{args.loc}.pkl")
-    results_dpath = os.path.join(os.path.expanduser("~"), "ground", f"results_modular_ablate_{args.ablate}" if args.ablate else "results_modular", loc_id)
-    os.makedirs(results_dpath, exist_ok=True)
-    srer_out_fname = "srer_outs.json"
-    srer_out_fpath = os.path.join(results_dpath, srer_out_fname)
-    reg_out_fpath = os.path.join(results_dpath, srer_out_fname.replace("srer", "reg"))
-    spg_out_fpath = os.path.join(results_dpath, srer_out_fname.replace("srer", "spg"))
-    lt_out_fpath = os.path.join(results_dpath, srer_out_fname.replace("srer", f"lt-{lt_id}"))
-    true_results_fpath = os.path.join(data_dpath, "dataset",f"{args.loc}_ablate" if args.ablate else f"{args.loc}", f"{loc_id}_true_results.json")
-    ltl_fpath = os.path.join(data_dpath, "dataset", "ltl_samples_sorted.csv")
 
     logging.basicConfig(level=logging.INFO,
                         format='%(message)s',
                         handlers=[
-                            logging.FileHandler(os.path.join(results_dpath, f"eval_results_modular_{args.module}.log"), mode='w'),
+                            logging.FileHandler(os.path.join(os.path.expanduser("~"), "ground", f"analyze_results_module_{args.module}.log"), mode='w'),
                             logging.StreamHandler()
                         ]
     )
-    logging.info(f"***** Modular-wise Evaluation Ablate {args.ablate}: {loc_id}\n" if args.ablate else f"***** Modular-wise Evaluation: {loc_id}\n")
-    logging.info(f"{graph_dpath}\n{osm_fpath}\n{utts_fpath}\n{true_results_fpath}\n{results_dpath}\n")
+    logging.info(f"***** Analyze Modular Evaluation Results: {args.module}\n")
 
-    if args.module == "srer" or args.module == "all":
-        srer_out_fpath_full = os.path.join(os.path.expanduser("~"), "ground", f"results_full_ablate_{args.ablate}" if args.ablate else "results_full", loc_id, srer_out_fname)
-        srer_out_fpath_ablate_txt = os.path.join(os.path.expanduser("~"), "ground", "results_full_ablate_text", loc_id, srer_out_fname)
-        srer_out_fpath_ablate_img = os.path.join(os.path.expanduser("~"), "ground", "results_full_ablate_image", loc_id, srer_out_fname)
-        srer_out_fpath_ablate_both = os.path.join(os.path.expanduser("~"), "ground", "results_full_ablate_both", loc_id, srer_out_fname)
-        if not os.path.isfile(srer_out_fpath) and os.path.isfile(srer_out_fpath_full):  # same SRER output for exp_full, exp_modular and ablate text
-            copy2(srer_out_fpath_full, srer_out_fpath)
-        elif not os.path.isfile(srer_out_fpath) and args.ablate and os.path.isfile(srer_out_fpath_ablate_txt):  # same SRER output for ablate text and ablate image
-            copy2(srer_out_fpath_ablate_txt, srer_out_fpath)
-        elif not os.path.isfile(srer_out_fpath) and args.ablate and os.path.isfile(srer_out_fpath_ablate_img):
-            copy2(srer_out_fpath_ablate_img, srer_out_fpath)
-        elif not os.path.isfile(srer_out_fpath) and args.ablate and os.path.isfile(srer_out_fpath_ablate_both):
-            copy2(srer_out_fpath_ablate_both, srer_out_fpath)
-        else:
-            run_exp_srer(utts_fpath, srer_out_fpath)
-        eval_srer(true_results_fpath, srer_out_fpath)
+    metric2ncorrects, metric2total = defaultdict(int), defaultdict(int)  # SRER: nprops vs. acc; REG: RE length vs. top-10 acc
 
-    if args.module == "reg" or args.module == "all":
-        run_exp_reg(true_results_fpath, graph_dpath, osm_fpath, args.topk, args.ablate, reg_out_fpath, reg_in_cache_fpath)
-        eval_reg(true_results_fpath, args.topk, reg_out_fpath)
+    for loc in ["providence", "auckland", "boston", "san_francisco"]:
+        for seed in [0, 1, 2, 42, 111 ]:
+            loc_id = f"{loc}_n{args.nsamples}_seed{seed}" if args.nsamples else f"{loc}_all_seed{seed}"
+            lt_id = f"lt-{args.lt}{args.nexamples}" if args.lt == "rag" else f"{args.lt}"
 
-    if args.module == "spg" or args.module == "all":
-        run_exp_spg(true_results_fpath, graph_dpath, osm_fpath, args.topk, rel_embeds_fpath, spg_out_fpath)
-        eval_spg(true_results_fpath, args.topk, spg_out_fpath)
+            data_dpath = os.path.join(os.path.expanduser("~"), "ground", "data")
+            results_dname = "results_modular_ablate_both" if seed == 111 else "results_modular"
+            results_dpath = os.path.join(os.path.expanduser("~"), "ground", results_dname, loc_id)
+            srer_out_fname = "srer_outs.json"
+            srer_out_fpath = os.path.join(results_dpath, srer_out_fname)
+            reg_out_fpath = os.path.join(results_dpath, srer_out_fname.replace("srer", "reg"))
+            spg_out_fpath = os.path.join(results_dpath, srer_out_fname.replace("srer", "spg"))
+            lt_out_fpath = os.path.join(results_dpath, srer_out_fname.replace("srer", f"lt-{lt_id}"))
+            true_results_dname = f"{loc}_ablate" if seed == 111 else f"{loc}"
+            true_results_fpath = os.path.join(data_dpath, "dataset", true_results_dname, f"{loc_id}_true_results.json")
 
-    if args.module == "lt" or args.module == "all":
-        if args.lt == "t5":
-            run_exp_lt(true_results_fpath, model_fpath, lt_out_fpath)
-        elif args.lt == "rag":
-            run_exp_lt_rag(true_results_fpath, lt_out_fpath, data_dpath, ltl_fpath, args.nexamples)
-        eval_lt(true_results_fpath, lt_out_fpath)
+            if args.module == "srer" or args.module == "all":
+                nprops2acc = eval_srer(true_results_fpath, srer_out_fpath)
+                for nprops, (ncorrects, ntotal) in nprops2acc.items():
+                    metric2ncorrects[nprops] += ncorrects
+                    metric2total[nprops] += ntotal
+
+            if args.module == "reg" or args.module == "all":
+                len2acc = eval_reg(true_results_fpath, args.topk, reg_out_fpath)
+                for re_len, (ncorrects, ntotal) in len2acc.items():
+                    metric2ncorrects[re_len] += ncorrects
+                    metric2total[re_len] += ntotal
+
+            if args.module == "spg" or args.module == "all":
+                len2acc = eval_spg(true_results_fpath, args.topk, spg_out_fpath)
+
+            if args.module == "lt" or args.module == "all":
+                eval_lt(true_results_fpath, lt_out_fpath)
+
+    if args.module == "srer":
+        nprops2acc = {nprops: ncorrects / metric2total[nprops] for nprops, ncorrects in metric2ncorrects.items()}
+        logging.info(f"SRER nprops vs. acc: {nprops2acc}")
+
+    if args.module == "reg":
+        len2acc = {re_len: ncorrects / metric2total[re_len] for re_len, ncorrects in metric2ncorrects.items()}
+        len2acc_sorted = sorted(len2acc.items(), key=lambda kv: kv[0])
+        len2acc_sorted = {len: acc for len, acc in len2acc_sorted}
+        logging.info(f"REG RE length vs. acc: {len2acc_sorted}")
